f4f23bf259dfeb051c6b19f22239b53c5fec0f08
elizaveta-dudnik <e.elizaveta.d@gmail.com>
2019-11-14 18:50:03 +0300

    added hw2

---
+++ hw2/Program8.4.pas
@@ -0,0 +1,13 @@
+﻿//8) Задача на цикл while. Дано целое число N (> 0). Если оно является степенью числа 3, то вывести True, если не является — вывести False. 
+
+var 
+  N: integer;
+begin;
+  readln(N);
+  while N > 0 do
+  begin
+    if N mod 3 = 0 then 
+    writeln('True')
+    else writeln('False');
+    end;
+end.
---
+++ hw2/Program9.4.pas
@@ -0,0 +1,16 @@
+﻿// 9) Даны два целых числа A и B (A < B). Найти сумму квадратов всех целых чисел от A до B включительно. 
+
+var 
+  A, B, sum: real;
+begin
+  readln(A, B);
+  sum := 1;
+  while A < B do 
+  begin
+    for A to B do
+    A := A * A;
+    B := B * B;
+    sum += A + B;
+  end;
+writeln(sum);
+end.
