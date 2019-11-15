ec17d3f2beeea24eecd03fb524cd78f3dd12445a
Victoria <viky.fomina@yandex.ru>
2019-11-15 11:57:02 +0300

    Added a folder with programs that were done in the class

---
+++ 14_11_19/Program8.4_remake.pas
@@ -0,0 +1,32 @@
+﻿//8) Задача на цикл while. Дано целое число N (> 0). Если оно является степенью числа 3, то вывести True, если не является — вывести False. 
+
+var 
+  N: integer;
+  numberIsPowerOf3: boolean;
+  
+begin;
+  readln(N);
+  
+  if N > 0 then
+  begin
+    numberIsPowerOf3 := false;
+    
+    while N <> 1 do
+    begin
+      if N mod 3 = 0 then
+      begin
+        numberIsPowerOf3 := true;
+        N := N div 3
+      end
+      else
+      begin
+        numberIsPowerOf3 := false;
+        break;
+      end;
+    end; 
+    
+    writeln(numberIsPowerOf3);
+  end
+  else
+    writeln('Wrong data');
+end.
---
+++ 14_11_19/Program9.4_remake.pas
@@ -0,0 +1,15 @@
+﻿// 9) Даны два целых числа A и B (A < B). Найти сумму квадратов всех целых чисел от A до B включительно. 
+
+var 
+  A, B, sum: real;
+begin
+  readln(A, B);
+  sum := 0;
+  
+  for var i := A to B do
+  begin
+   sum += i * i;
+  end;
+  
+  writeln(sum);
+end.
