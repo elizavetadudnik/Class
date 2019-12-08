99356da1e5672555d2967dc774f587467043b519
Victoria <viky.fomina@yandex.ru>
2019-12-08 16:39:16 +0300

    Added new classwork

---
+++ 08_12_2019/Program1.pas
@@ -0,0 +1,17 @@
+﻿// Школьное 1
+
+var
+  number: integer;
+  
+begin
+  readln(number);
+  
+  for var i := 1 to number do
+  begin
+    for var j := 1 to i do
+    begin
+      write('*');
+    end;
+    writeln();
+  end;
+end.
---
+++ 08_12_2019/Program2.pas
@@ -0,0 +1,35 @@
+﻿// Переставить местами первую и последнюю цифры натурального числа.
+
+var
+  number, currentNumber, firstDigit, lastDigit, numberOfDigits, power10, expression: integer;
+  
+begin
+  readln(number);
+  
+  if number >= 10 then
+  begin
+    lastDigit := number mod 10;
+    currentNumber := number;
+    numberOfDigits := 0;
+    
+    while currentNumber > 0 do
+    begin
+      currentNumber := currentNumber div 10;
+      numberOfDigits += 1;
+    end;
+    
+    power10 := 1;
+    
+    for var i := 1 to numberOfDigits - 1 do
+    begin
+      power10 *= 10;    
+    end;  
+    
+    firstDigit := number div power10;
+    expression := number - lastDigit + firstDigit;
+    expression -= power10;
+    expression += lastDigit * power10;
+    
+    writeln(expression);
+  end;
+end.
