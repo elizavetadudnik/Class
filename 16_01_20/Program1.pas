e9d68946c4d096d70092b10e0783d429750149db
Victoria <viky.fomina@yandex.ru>
2020-01-16 16:46:39 +0300

    added classwork

---
+++ 16_01_20/Program1.pas
@@ -0,0 +1,22 @@
+﻿// Массив заполнен случайными числами от -5 до 5. Найти и вывести на экран кол-во эл-тов, кратных 9.
+
+const 
+  size = 18;
+
+var
+  arr: array[1..size] of integer;
+  count: integer;
+  
+begin
+  count := 0;
+
+  for var i := 1 to size do
+  begin
+    arr[i] := random(-5, 5);
+    
+    if arr[i] mod 9 = 0 then   
+      count += 1;
+  end;
+  
+  writeln(count)
+end.
---
+++ 16_01_20/Program2.pas
@@ -0,0 +1,13 @@
+﻿// Массив с клавы. Вывести элементы, кратные 5, стоящие на нечетных местах.
+
+var 
+  arr: array [1..10] of integer;
+  
+begin
+  for var i := 1 to 10 do
+  begin
+    readln(arr[i]);
+    if (arr[i] mod 5 = 0) and (i mod 2 = 1) then
+      writeln(arr[i]);
+  end;
+end.
