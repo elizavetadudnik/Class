d9e1d1a136326820570ba0d79639f518fa94d528
Victoria <viky.fomina@yandex.ru>
2020-02-06 17:45:06 +0300

    added tasks

---
+++ 06_02_20/Program4.pas
@@ -0,0 +1,39 @@
+﻿const
+  size = 6;
+  
+var
+  arr: array[1..size] of integer;
+  onlyZeros, positiveEarlier, negativeEarlier: boolean;
+  
+begin
+  for var i := 1 to size do
+    readln(arr[i]);
+    
+  writeln(arr);
+  
+  positiveEarlier := false;
+  negativeEarlier := false;
+  
+  for var i := 1 to size do
+  begin
+    if arr[i] > 0 then
+    begin
+      positiveEarlier := true;
+      break;
+    end
+    else if arr[i] < 0 then
+    begin
+      negativeEarlier := true;
+      break;
+    end;
+  end;
+  
+  onlyZeros := (positiveEarlier = false) and (negativeEarlier = false);
+  
+  if onlyZeros then
+    writeln('только нули')
+  else if positiveEarlier then
+    writeln('положительное раньше')
+  else
+    writeln('отрицательное раньше')
+end.
