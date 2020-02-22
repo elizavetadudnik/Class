6a230551a56cf9803f69370fab0a1a90d8eac2de
Victoria <viky.fomina@yandex.ru>
2020-02-22 21:11:07 +0300

    added classwork

---
+++ 22_02_20/Program3.pas
@@ -0,0 +1,33 @@
+﻿const
+  rowSize = 10;
+  columnSize = 10;
+
+var
+  arr: array[1..rowSize, 1..columnSize] of integer;
+  
+begin
+  for var i := 1 to rowSize do
+  begin
+    for var j :=  1 to columnSize do
+    begin
+        arr[i, j] := 0;
+    end;
+  end;
+  
+  for var i := 1 to rowSize do
+  begin
+    for var j := i to columnSize do
+    begin
+      arr[i, j] := j;
+    end;
+  end;
+  
+  for var i := 1 to rowSize do
+  begin
+    for var j :=  1 to columnSize do
+    begin
+        write(arr[i, j], ' ');
+    end;
+    writeln();
+  end; 
+end.
