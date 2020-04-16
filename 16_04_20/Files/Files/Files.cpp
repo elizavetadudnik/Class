d318e2c4f87e07ea49f878d210250d7a3cd3a1b4
Victoria <viky.fomina@yandex.ru>
2020-04-16 16:42:11 +0300

    work with files

---
+++ 16_04_20/Files/Files/Files.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    fstream file;
+    file.open("file.txt");
+
+    if (file.is_open())
+    {
+        file << "The weather is good" << endl; // запись в файл
+        file.close();
+    }
+    else
+    {
+        cout << "File was not open!" << endl;
+    }
+
+    file.open("file.txt");
+    if (file.is_open())
+    {
+        int const size = 50;
+        char buffer[size];
+
+        file >> buffer;
+        cout << buffer << endl;
+        file >> buffer;
+        cout << buffer << endl;
+
+        file.getline(buffer, 50);
+        cout << buffer << endl;
+    }
+}
